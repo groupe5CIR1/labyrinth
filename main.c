157c26f6a19afd8a5a087956ec20a720a6681b78
Valentin TANG-PATUREL <valentin.tang-paturel@isen-ouest.yncrea.fr>
2025-09-15 15:19:12 +0200

    fonctions jsp ntm

--- main.c
+++ main.c
@@ -7,6 +7,25 @@ int main(void) {
 }
 
 
+
 int random(int n) {
     return rand()%n;
 }
+
+struct Grid init_grid() {
+
+}
+
+struct Cell init_cell(int adjacents) {
+
+}
+
+void updateNeighbours(struct Grid* grid, struct Cell* cell) {
+
+}
+
+int choose_path(int adjacents) {
+
+}
+
+
